576a5fabef88072ae43038ad7d0d647339e83d2e
Bill <sanatillayevbilol@gmail.com>
2024-06-09 17:15:35 +0500

    Initial Commit

---
+++ FourInOne/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FourInOne
+//
+//  Created by Bilol Sanatillayev on 09/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FourInOne/FourInOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FourInOneApp.swift
+//  FourInOne
+//
+//  Created by Bilol Sanatillayev on 09/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FourInOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
